2bd2d02f92d373e632ca6e30a0100b44106f80a0
HosseinHzp <hosseinhzp@gmail.com>
2025-11-05 18:57:38 +0330

    your message

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -163,7 +163,7 @@ export default function Home() {
             </div>
 
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-4">
-              <Card>
+              <Card className="hidden md:flex">
                 <CardHeader>
                   <CardTitle>Calendar</CardTitle>
                   <CardDescription>Check your events and tasks</CardDescription>
